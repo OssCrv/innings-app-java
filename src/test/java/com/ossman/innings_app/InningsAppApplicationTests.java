eb077e94cc73111317d4f9126ab888e8100199be
Ossman O. C. <carvajalossman@gmail.com>
2025-10-16 12:51:21 -0500

    Inicializando proyecto

---
+++ src/test/java/com/ossman/innings_app/InningsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ossman.innings_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InningsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
